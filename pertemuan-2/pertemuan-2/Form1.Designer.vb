eece9b4c483049e691fc50eee8e09f2b4cd64117
Fathii3 <fathifadhil996@gmail.com>
2026-09-01 10:01:16 +0700

    Revert perubahan pertemuan-2 ke versi awal

--- pertemuan-2/pertemuan-2/Form1.Designer.vb
+++ pertemuan-2/pertemuan-2/Form1.Designer.vb
@@ -1,4 +1,4 @@
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,209 +22,116 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        pnlHeader = New Panel()
-        lblHeaderSub = New Label()
-        lblHeaderTitle = New Label()
-        pnlCard = New Panel()
-        btnKeluar = New Button()
-        btnHapus = New Button()
-        btnTampilkan = New Button()
-        txtKom = New TextBox()
-        lblKom = New Label()
+        spnnama = New Label()
+        txtNama = New TextBox()
         txtNim = New TextBox()
         lblNim = New Label()
-        txtNama = New TextBox()
-        spnnama = New Label()
-        pnlHeader.SuspendLayout()
-        pnlCard.SuspendLayout()
+        lblKom = New Label()
+        txtKom = New TextBox()
+        btnTampilkan = New Button()
+        btnHapus = New Button()
+        btnKeluar = New Button()
         SuspendLayout()
-        '
-        ' pnlHeader
-        '
-        pnlHeader.BackColor = Color.FromArgb(37, 99, 235)
-        pnlHeader.Controls.Add(lblHeaderSub)
-        pnlHeader.Controls.Add(lblHeaderTitle)
-        pnlHeader.Dock = DockStyle.Top
-        pnlHeader.Location = New Point(0, 0)
-        pnlHeader.Name = "pnlHeader"
-        pnlHeader.Size = New Size(484, 80)
-        pnlHeader.TabIndex = 0
-        '
-        ' lblHeaderSub
-        '
-        lblHeaderSub.AutoSize = True
-        lblHeaderSub.Font = New Font("Segoe UI", 9.0F)
-        lblHeaderSub.ForeColor = Color.FromArgb(219, 234, 254)
-        lblHeaderSub.Location = New Point(24, 44)
-        lblHeaderSub.Name = "lblHeaderSub"
-        lblHeaderSub.Size = New Size(295, 20)
-        lblHeaderSub.TabIndex = 1
-        lblHeaderSub.Text = "Silakan lengkapi formulir identitas di bawah ini"
-        '
-        ' lblHeaderTitle
-        '
-        lblHeaderTitle.AutoSize = True
-        lblHeaderTitle.Font = New Font("Segoe UI Semibold", 13.0F, FontStyle.Bold)
-        lblHeaderTitle.ForeColor = Color.White
-        lblHeaderTitle.Location = New Point(23, 14)
-        lblHeaderTitle.Name = "lblHeaderTitle"
-        lblHeaderTitle.Size = New Size(244, 30)
-        lblHeaderTitle.TabIndex = 0
-        lblHeaderTitle.Text = "Data Profil Mahasiswa"
-        '
-        ' pnlCard
-        '
-        pnlCard.BackColor = Color.White
-        pnlCard.Controls.Add(btnKeluar)
-        pnlCard.Controls.Add(btnHapus)
-        pnlCard.Controls.Add(btnTampilkan)
-        pnlCard.Controls.Add(txtKom)
-        pnlCard.Controls.Add(lblKom)
-        pnlCard.Controls.Add(txtNim)
-        pnlCard.Controls.Add(lblNim)
-        pnlCard.Controls.Add(txtNama)
-        pnlCard.Controls.Add(spnnama)
-        pnlCard.Location = New Point(25, 100)
-        pnlCard.Name = "pnlCard"
-        pnlCard.Size = New Size(434, 315)
-        pnlCard.TabIndex = 1
-        '
+        ' 
+        ' spnnama
+        ' 
+        spnnama.AutoSize = True
+        spnnama.Location = New Point(102, 27)
+        spnnama.Name = "spnnama"
+        spnnama.Size = New Size(56, 20)
+        spnnama.TabIndex = 0
+        spnnama.Text = "Nama :"
+        ' 
+        ' txtNama
+        ' 
+        txtNama.Location = New Point(176, 27)
+        txtNama.Name = "txtNama"
+        txtNama.Size = New Size(125, 27)
+        txtNama.TabIndex = 1
+        ' 
+        ' txtNim
+        ' 
+        txtNim.Location = New Point(176, 85)
+        txtNim.Name = "txtNim"
+        txtNim.Size = New Size(125, 27)
+        txtNim.TabIndex = 2
+        ' 
+        ' lblNim
+        ' 
+        lblNim.AutoSize = True
+        lblNim.Location = New Point(114, 85)
+        lblNim.Name = "lblNim"
+        lblNim.Size = New Size(44, 20)
+        lblNim.TabIndex = 3
+        lblNim.Text = "NIM :"
+        ' 
+        ' lblKom
+        ' 
+        lblKom.AutoSize = True
+        lblKom.Location = New Point(110, 146)
+        lblKom.Name = "lblKom"
+        lblKom.Size = New Size(48, 20)
+        lblKom.TabIndex = 5
+        lblKom.Text = "KOM :"
+        ' 
+        ' txtKom
+        ' 
+        txtKom.Location = New Point(176, 143)
+        txtKom.Name = "txtKom"
+        txtKom.Size = New Size(125, 27)
+        txtKom.TabIndex = 4
+        ' 
+        ' btnTampilkan
+        ' 
+        btnTampilkan.Location = New Point(224, 216)
+        btnTampilkan.Name = "btnTampilkan"
+        btnTampilkan.Size = New Size(94, 29)
+        btnTampilkan.TabIndex = 6
+        btnTampilkan.Text = "Tampilkan"
+        btnTampilkan.UseVisualStyleBackColor = True
+        ' 
+        ' btnHapus
+        ' 
+        btnHapus.Location = New Point(339, 216)
+        btnHapus.Name = "btnHapus"
+        btnHapus.Size = New Size(94, 29)
+        btnHapus.TabIndex = 7
+        btnHapus.Text = "Hapus"
+        btnHapus.UseVisualStyleBackColor = True
+        ' 
         ' btnKeluar
-        '
-        btnKeluar.BackColor = Color.FromArgb(254, 242, 242)
-        btnKeluar.Cursor = Cursors.Hand
-        btnKeluar.FlatAppearance.BorderColor = Color.FromArgb(254, 202, 202)
-        btnKeluar.FlatStyle = FlatStyle.Flat
-        btnKeluar.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular)
-        btnKeluar.ForeColor = Color.FromArgb(220, 38, 38)
-        btnKeluar.Location = New Point(310, 242)
+        ' 
+        btnKeluar.Location = New Point(275, 274)
         btnKeluar.Name = "btnKeluar"
-        btnKeluar.Size = New Size(95, 38)
+        btnKeluar.Size = New Size(94, 29)
         btnKeluar.TabIndex = 8
         btnKeluar.Text = "Keluar"
-        btnKeluar.UseVisualStyleBackColor = False
-        '
-        ' btnHapus
-        '
-        btnHapus.BackColor = Color.FromArgb(241, 245, 249)
-        btnHapus.Cursor = Cursors.Hand
-        btnHapus.FlatAppearance.BorderColor = Color.FromArgb(203, 213, 225)
-        btnHapus.FlatStyle = FlatStyle.Flat
-        btnHapus.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular)
-        btnHapus.ForeColor = Color.FromArgb(71, 85, 105)
-        btnHapus.Location = New Point(195, 242)
-        btnHapus.Name = "btnHapus"
-        btnHapus.Size = New Size(105, 38)
-        btnHapus.TabIndex = 7
-        btnHapus.Text = "Bersihkan"
-        btnHapus.UseVisualStyleBackColor = False
-        '
-        ' btnTampilkan
-        '
-        btnTampilkan.BackColor = Color.FromArgb(37, 99, 235)
-        btnTampilkan.Cursor = Cursors.Hand
-        btnTampilkan.FlatAppearance.BorderSize = 0
-        btnTampilkan.FlatStyle = FlatStyle.Flat
-        btnTampilkan.Font = New Font("Segoe UI Semibold", 9.5F, FontStyle.Bold)
-        btnTampilkan.ForeColor = Color.White
-        btnTampilkan.Location = New Point(25, 242)
-        btnTampilkan.Name = "btnTampilkan"
-        btnTampilkan.Size = New Size(160, 38)
-        btnTampilkan.TabIndex = 6
-        btnTampilkan.Text = "Tampilkan Data"
-        btnTampilkan.UseVisualStyleBackColor = False
-        '
-        ' txtKom
-        '
-        txtKom.Font = New Font("Segoe UI", 10.0F)
-        txtKom.Location = New Point(25, 185)
-        txtKom.Name = "txtKom"
-        txtKom.PlaceholderText = "Contoh: KOM C"
-        txtKom.Size = New Size(380, 30)
-        txtKom.TabIndex = 5
-        '
-        ' lblKom
-        '
-        lblKom.AutoSize = True
-        lblKom.Font = New Font("Segoe UI Semibold", 9.0F, FontStyle.Bold)
-        lblKom.ForeColor = Color.FromArgb(51, 65, 85)
-        lblKom.Location = New Point(25, 160)
-        lblKom.Name = "lblKom"
-        lblKom.Size = New Size(95, 20)
-        lblKom.TabIndex = 4
-        lblKom.Text = "KOM / Kelas"
-        '
-        ' txtNim
-        '
-        txtNim.Font = New Font("Segoe UI", 10.0F)
-        txtNim.Location = New Point(25, 115)
-        txtNim.Name = "txtNim"
-        txtNim.PlaceholderText = "Contoh: 231401001"
-        txtNim.Size = New Size(380, 30)
-        txtNim.TabIndex = 3
-        '
-        ' lblNim
-        '
-        lblNim.AutoSize = True
-        lblNim.Font = New Font("Segoe UI Semibold", 9.0F, FontStyle.Bold)
-        lblNim.ForeColor = Color.FromArgb(51, 65, 85)
-        lblNim.Location = New Point(25, 90)
-        lblNim.Name = "lblNim"
-        lblNim.Size = New Size(224, 20)
-        lblNim.TabIndex = 2
-        lblNim.Text = "NIM (Nomor Induk Mahasiswa)"
-        '
-        ' txtNama
-        '
-        txtNama.Font = New Font("Segoe UI", 10.0F)
-        txtNama.Location = New Point(25, 45)
-        txtNama.Name = "txtNama"
-        txtNama.PlaceholderText = "Masukkan nama lengkap"
-        txtNama.Size = New Size(380, 30)
-        txtNama.TabIndex = 1
-        '
-        ' spnnama
-        '
-        spnnama.AutoSize = True
-        spnnama.Font = New Font("Segoe UI Semibold", 9.0F, FontStyle.Bold)
-        spnnama.ForeColor = Color.FromArgb(51, 65, 85)
-        spnnama.Location = New Point(25, 20)
-        spnnama.Name = "spnnama"
-        spnnama.Size = New Size(113, 20)
-        spnnama.TabIndex = 0
-        spnnama.Text = "Nama Lengkap"
-        '
+        btnKeluar.UseVisualStyleBackColor = True
+        ' 
         ' Form1
-        '
-        AcceptButton = btnTampilkan
+        ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        BackColor = Color.FromArgb(241, 245, 249)
-        CancelButton = btnKeluar
-        ClientSize = New Size(484, 440)
-        Controls.Add(pnlCard)
-        Controls.Add(pnlHeader)
-        Font = New Font("Segoe UI", 9.0F, FontStyle.Regular)
-        FormBorderStyle = FormBorderStyle.FixedSingle
-        MaximizeBox = False
+        ClientSize = New Size(672, 362)
+        Controls.Add(btnKeluar)
+        Controls.Add(btnHapus)
+        Controls.Add(btnTampilkan)
+        Controls.Add(lblKom)
+        Controls.Add(txtKom)
+        Controls.Add(lblNim)
+        Controls.Add(txtNim)
+        Controls.Add(txtNama)
+        Controls.Add(spnnama)
         Name = "Form1"
-        StartPosition = FormStartPosition.CenterScreen
-        Text = "Sistem Profil Mahasiswa"
-        pnlHeader.ResumeLayout(False)
-        pnlHeader.PerformLayout()
-        pnlCard.ResumeLayout(False)
-        pnlCard.PerformLayout()
+        Text = "Profile Mahasiswa"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
-    Friend WithEvents pnlHeader As Panel
-    Friend WithEvents lblHeaderTitle As Label
-    Friend WithEvents lblHeaderSub As Label
-    Friend WithEvents pnlCard As Panel
     Friend WithEvents spnnama As Label
     Friend WithEvents txtNama As TextBox
-    Friend WithEvents lblNim As Label
     Friend WithEvents txtNim As TextBox
+    Friend WithEvents lblNim As Label
     Friend WithEvents lblKom As Label
     Friend WithEvents txtKom As TextBox
     Friend WithEvents btnTampilkan As Button
--- pertemuan-2/pertemuan-2/Form1.vb
+++ pertemuan-2/pertemuan-2/Form1.vb
@@ -1,40 +1,23 @@
-Public Class Form1
+﻿Public Class Form1
+    Private Sub spnnama_Click(sender As Object, e As EventArgs) Handles spnnama.Click
+
+    End Sub
+
     Private Sub btnTampilkan_Click(sender As Object, e As EventArgs) Handles btnTampilkan.Click
-        ' Validasi input sederhana agar lebih ramah pengguna
-        If String.IsNullOrWhiteSpace(txtNama.Text) OrElse
-           String.IsNullOrWhiteSpace(txtNim.Text) OrElse
-           String.IsNullOrWhiteSpace(txtKom.Text) Then
-
-            MessageBox.Show("Mohon lengkapi semua data (Nama, NIM, dan KOM) sebelum menampilkan!",
-                            "Peringatan",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Warning)
-            Return
-        End If
-
-        Dim pesan As String = "Informasi Profil Mahasiswa:" & vbCrLf & vbCrLf &
-                              "Nama : " & txtNama.Text.Trim() & vbCrLf &
-                              "NIM  : " & txtNim.Text.Trim() & vbCrLf &
-                              "KOM  : " & txtKom.Text.Trim()
-
-        MessageBox.Show(pesan, "Data Berhasil Dimuat", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("Selamat Datang" & vbCrLf &
+            "Nama   : " & txtNama.Text & vbCrLf &
+            "NIM    : " & txtNim.Text & vbCrLf &
+            "KOM    : " & txtKom.Text
+            )
     End Sub
 
     Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
         txtNama.Clear()
         txtNim.Clear()
         txtKom.Clear()
-        txtNama.Focus()
     End Sub
 
     Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
-        Dim konfirmasi = MessageBox.Show("Apakah Anda yakin ingin keluar dari aplikasi?",
-                                         "Konfirmasi Keluar",
-                                         MessageBoxButtons.YesNo,
-                                         MessageBoxIcon.Question)
-
-        If konfirmasi = DialogResult.Yes Then
-            Me.Close()
-        End If
+        Me.Close()
     End Sub
 End Class
